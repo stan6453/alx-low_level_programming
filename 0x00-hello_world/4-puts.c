94d993824fc607879444910f21c16c6f4b80ee57
Ezechukwu Stanley <stanley6453@gmail.com>
2022-09-08 05:45:41 -0700

    added new files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void){
+/**
+ * function_name - Short description, single line
+ * Description: Longer description of the function)?
+ * Return: Description of the returned value
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
