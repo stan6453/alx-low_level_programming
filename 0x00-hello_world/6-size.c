bf8f1c74e8b07938f2e7bbda977617caf69f97dd
Ezechukwu Stanley <stanley6453@gmail.com>
2022-09-08 02:08:32 -0700

    added new files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a double: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size a of char: %zu byte(s)\n", sizeof(charType));
+
+	return (0);
