050ce6b25bd34822d37522a6a97fd45cb5960229
Ezechukwu Stanley <stanley6453@gmail.com>
2022-09-08 05:46:46 -0700

    added new files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * function_name - Short description, single line
+ * main - Short description, single line
  * Description: Longer description of the function)?
  * Return: Description of the returned value
  */
