c7f52f3fde1b280fa407efefbd3ab16c5a480c9c
Ezechukwu Stanley <stanley6453@gmail.com>
2022-09-08 01:13:06 -0700

    added new files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-/* main function */
+/**
+	 * This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+	 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
