6d5bbef9a8c32881d6928f15e4311b8778ade937
Ezechukwu Stanley <stanley6453@gmail.com>
2022-09-09 14:33:32 -0700

    restord 101-print_comb4.c that I mistakenly overwrote

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,37 +7,35 @@
  */
 int main(void)
 {
-	int i, j;
-	int a, b, c, d;
+	int i, j, k;
 
-	for (i = 0; i < 100; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		a = i / 10; 
-		b = i % 10;
-
-		for (j = 0; j < 100; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			c = j / 10;
-			b = j % 10;
-
-			if ( a < c || (a == c && b < d))
+			for (k = '0'; k <= '9'; k++)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(' ');
-				putchar(c + '0');
-				putchar(d + '0');
-
-				if(!(a == 9 && b == 8 && c == 9 && d == 9))
+				if ((i < j && j < k) && (i != j && j != k && k != i))
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
 				}
+
 			}
 
 		}
+
+
 	}
+
 	putchar('\n');
 	return (0);
-
 }
