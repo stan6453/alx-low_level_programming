1cdf12534c1d011ecb57e2a32c303b078563136c
Ezechukwu Stanley <stanley6453@gmail.com>
2022-09-09 06:40:25 -0700

    added file 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+/**
+ *main - program execution starts here
+ *Description: this is the main function
+ *Return: an integer to signify success or failure
+ */
+int main(void)
+{
+	int i, j, k, l, x_value, y_value;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+				{
+					x_value = i + j;
+					y_value = k + l;
+					if (x_value < y_value && x_value != y_value)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+
+						if (i != '9' || j != '8' || k != '9' || l != '9')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
