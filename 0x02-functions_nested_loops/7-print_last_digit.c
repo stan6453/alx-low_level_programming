b2295c0d5facb3f59a140bd6da3312002d7d50c4
Ezechukwu Stanley <stanley6453@gmail.com>
2022-09-13 04:34:42 -0700

    modified file 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  *print_last_digit - prints the last digit of a fn
@@ -8,7 +9,7 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit = abs(n % 10);
 
 	_putchar(last_digit + '0');
 	return (last_digit);
