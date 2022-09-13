40d5007041747d7cb2568bd06675e8cd344fb7d5
Ezechukwu Stanley <stanley6453@gmail.com>
2022-09-13 08:02:50 -0700

    updated file 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,7 +23,7 @@ void print_times_table(int n)
 					if (j != 0 )
 						printf("%4d,", product);
 					else
-						printf("%d",product);
+						printf("%d,",product);
 				}
 				else
 					printf("%4d", i * j);
