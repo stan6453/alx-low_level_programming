b17f52828df211950fa8804e6c151b06ef5af853
Ezechukwu Stanley <stanley6453@gmail.com>
2022-09-19 01:14:57 -0700

    modified file 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,10 +5,10 @@ char *_strcpy(char *dest, char *src)
 	int index;
 	int letter;
 
-	for(index = 0; ((letter = src[index]) != '\0'); index++)
+	for (index = 0; ((letter = src[index]) != '\0'); index++)
 	{
 		dest[index] = letter;
 	}
 	dest[index] = letter;
-	return dest;
+	return (dest);
 }
