3fe7a20845680d96566bc10e6c75de2b7307bf96
Stanley <stanley@kali.kali>
2022-09-22 09:41:01 +0100

    modified some files

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+
+
+/**
+ * rot13 - multiplies two digits
+ * @str: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = str[i] + 13;
+
+		if (str[i] > 'z')
+			str[i] = str[((str[i] - 'z') + 'a') - 1];
+	}
+	return(str)
+}
