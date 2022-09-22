f6cdb70b2a8b5ed592f4c90ee8e372bfc7f6e88a
Stanley <stanley@kali.kali>
2022-09-22 02:17:33 +0100

    modified some files

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,12 @@
 
 
-
+/**
+ * reverse_array - multiplies two digits
+ * @a: 1st digit in the multiplication
+ * @n: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
 void reverse_array(int *a, int n)
 {
 	int i, j, temp;
