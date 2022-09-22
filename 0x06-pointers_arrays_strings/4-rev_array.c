0035dd39538de557eeac80dce04c889cf9c46dc0
Stanley <stanley@kali.kali>
2022-09-22 02:22:36 +0100

    modified some files

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,9 +15,9 @@ void reverse_array(int *a, int n)
 	j = n - 1;
 	while (i < j)
 	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
 		i++;
 		j--;
 	}
