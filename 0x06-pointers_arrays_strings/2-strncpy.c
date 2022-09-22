167ceb68c67068f4bff792996148420ebab4efbd
Stanley <stanley@kali.kali>
2022-09-22 01:57:40 +0100

    modified some files

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,22 @@
 
-
+/**
+ * _strncpy - multiplies two digits
+ * @dest 1st digit in the multiplication
+ * @src 1st digit in the multiplication
+ * @n: 2nd digit to be multiplied
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 
 	i = j = 0;
-	while(1)
+	while (1)
 	{
-		while ((dest[i++] = src[j++]) != '\0' && i-2 !=  n) /* copy src */
+		while ((dest[i++] = src[j++]) != '\0' && i !=  n) /* copy src */
 			;
-		if(i-2 == n)
+		if (i == n)
 			break;
 		j = 0;
 	}
