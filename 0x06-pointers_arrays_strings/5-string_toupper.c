255a5c7d32eee38a60ab59a6270c1017e889d11a
Stanley <stanley@kali.kali>
2022-09-22 02:29:29 +0100

    modified some files

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,11 @@
 
 
-
+/**
+ * string_toupper - multiplies two digits
+ * @str: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
 char *string_toupper(char *str)
 {
 	int i = 0, letter;
@@ -8,9 +13,9 @@ char *string_toupper(char *str)
 	while (str[i] != '\0')
 	{
 		letter = str[i];
-		if (letter >='a' && letter <='z')
+		if (letter >= 'a' && letter <= 'z')
 			str[i] = letter - 32;
-		i++
+		i++;
 	}
 	return (str);
 }
