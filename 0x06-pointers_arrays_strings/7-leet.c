9457a415d9c81cf48ad347f9154e79d7ad9378f2
Stanley <stanley@kali.kali>
2022-09-22 09:31:24 +0100

    modified some files

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,33 @@
 
 
-
+/**
+ * leet - multiplies two digits
+ * @str: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
 char *leet(char *str)
 {
-	int i = 0;
+	int letter, i = 0;
 
 	while (str[i] != '\0')
 	{
+		letter = str[i];
+		if (letter == 'A' || letter == 'a')
+			str[i] = '4';
 
+		if (letter == 'e' || letter == 'E')
+			str[i] = '3';
+
+		if (letter == 'o' || letter == 'O')
+			str[i] = '0';
+
+		if (letter == 't' || letter == 'T')
+			str[i] = '7';
+
+		if (letter == 'l' || letter == 'L')
+			str[i] = '1';
+		i++;
 	}
+	return (str)
 }
