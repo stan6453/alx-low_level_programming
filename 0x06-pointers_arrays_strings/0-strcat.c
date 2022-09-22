d58fac76947ecc336add061fd833c12fce3e457e
Stanley <stanley@kali.kali>
2022-09-22 01:28:06 +0100

    updated some files

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 
 /**
  * _strcat - multiplies two digits
- * @n: 1st digit in the multiplication
- * @n: 2nd digit to be multiplied
+ * @src: 1st digit in the multiplication
+ * @dest: 2nd digit to be multiplied
  * Description: multiplies two digits
  * Return: the result of the multiplication
  */
@@ -10,7 +10,8 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	i = j = 0;
+	i = 0;
+	j = 0;
 	while (dest[i] != '\0') /* find end of dest */
 		i++;
 	while ((dest[i++] = src[j++]) != '\0') /* copy src */
