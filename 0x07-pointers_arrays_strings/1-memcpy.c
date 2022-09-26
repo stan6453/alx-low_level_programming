628ca9fbbe4958f717afeff793adbbfa6d87e16a
Stanley <stanley@kali.kali>
2022-09-26 09:38:39 +0100

    added some files

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+
+/**
+ * _memcpy - multiplies two digits
+ * @dest: 1st digit in the multiplication
+ * @src: 1st digit in the multiplication
+ * @n: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
