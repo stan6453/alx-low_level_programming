45dab04e7191550ebebbc4077ad4b9070c787c1a
Stanley <stanley@kali.kali>
2022-09-26 12:06:49 +0100

    modified some files

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_chessboard - multiplies two digits
+ * @a: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		putchar('\n');
+	}
+}
