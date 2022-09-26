a07a370fe8ebb08aedcdb32727a6cb2d3fd7b79d
Stanley <stanley@kali.kali>
2022-09-26 12:11:09 +0100

    modified some files

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * _strchr - multiplies two digits
  * @s: 1st digit in the multiplication
@@ -8,6 +9,7 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
