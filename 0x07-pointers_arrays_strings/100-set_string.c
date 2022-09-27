5b260361e600bad285675f125dc93e4be3418228
stanley <stanley@kali.kara>
2022-09-27 06:40:39 +0100

    added file 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+
+
+/**
+ * set_string - multiplies two digits
+ * @s: 1st digit in the multiplication
+ * @to: 2nd digit to be multiplied
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+
+void set_string(char **s, char *to)
+{
+	while ((**s++ = *to++) !=  '\0')
+		;
+}
