78c0768b1ee03ee33c50c2863b5dbe6ef0705def
stanley <stanley@kali.kara>
2022-09-28 17:23:02 +0100

    added 1-print_rev_recursion.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+
+
+/**
+ * find_prime - multiplies two digits
+ * @n: 1st digit in the multiplication
+ * @num: 2nd digit to be multiplied
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+int find_prime(n, num)
+{
+	if (num == n)
+		return (1);
+	if (num % n == 0)
+		return (0);
+	return (find_prime(n, ++num));
+}
+
+
+/**
+ * is_prime_number - multiplies two digits
+ * @n: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return 0;
+	return (find_prime(n, 2))
+}
