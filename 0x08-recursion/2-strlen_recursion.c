2e5bef0e1c43b97bc0ced016d3fa09a3760c68a3
stanley <stanley@kali.kara>
2022-09-28 08:01:22 +0100

    added 1-print_rev_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  int _strlen_recursion - multiplies two digits
+ * @s: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + int _strlen_recursion(++s));
+	}
+	else
+		return (0)
+}
