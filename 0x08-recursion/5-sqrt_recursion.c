cc5e6f3d946afde6ca39d309b8c0e3a97a49328e
stanley <stanley@kali.kara>
2022-09-28 17:12:39 +0100

    added 1-print_rev_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,22 @@
 
-int num = 0;
+/**
+ * find_square - multiplies two digits
+ * @n: 1st digit in the multiplication
+ * @num: 2nd digit to be multiplied
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+int find_square(int n, int num)
+{
+	
+	if (num * num == n)
+		return num;
+	if (num * num > n)
+		return (-1);
+	return (find_square(n, ++num));
+}
+
+
 /**
  * _sqrt_recursion - multiplies two digits
  * @n: 1st digit in the multiplication
@@ -8,19 +25,6 @@ int num = 0;
  */
 int _sqrt_recursion(int n)
 {
-	int temp;
-
-	num++;
-	if (num == n - 1 || n < 0)
-	{
-		num = 0;
-		return (-1);
-	}
-	if (num * num == n)
-	{
-		temp = num;
-		num = 0;
-		return (temp);
-	}
-	return (_sqrt_recursion(n));
+	
+	return (find_square(n, 1));
 }
