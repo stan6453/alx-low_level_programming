f3dad359883888e2256142af460d13fa6ca4c565
stanley <stanley@kali.kara>
2022-09-28 07:46:57 +0100

    added 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+
+
+/**
+ *  _print_rev_recursion - multiplies two digits
+ * @s: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
