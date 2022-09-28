78a5ee6df342112cd7268f47cb9bc9ceb10d7820
stanley <stanley@kali.kara>
2022-09-28 08:29:52 +0100

    added 1-print_rev_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+
+
+/**
+ * _pow_recursion - multiplies two digits
+ * @x: 1st digit in the multiplication
+ * @y: 2nd digit to be multiplied
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
