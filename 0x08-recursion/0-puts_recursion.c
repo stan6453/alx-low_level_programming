a8b44e4ef773244fd97f5a4aee6fa091da3ca7c5
stanley <stanley@kali.kara>
2022-09-28 07:37:45 +0100

    added readme and 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+
+/**
+ * _puts_recursion - multiplies two digits
+ * @s: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+# This folder contains my solution to the ALX SE project on 0x08. C - Recursion
