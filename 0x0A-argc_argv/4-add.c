f88dacde2e0bac80ccaa1b469f4cd0689e888fe1
stanley <stanley@kali.kara>
2022-09-30 15:48:54 +0100

    modified file 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * is0nlydigit - checks if a string contains only digits
+ * isonlydigit - checks if a string contains only digits
  * @s: string to be checked
  * Description: checks if a string contains only digit
  * Return: 1 if string contains only didgit
- *         0 if string has at lest one non-digit character 
+ *         0 if string has at lest one non-digit character
  */
 int isonlydigit(char *s)
 {
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		if (*s < '0' || *s > '9')
 			return (0);
