a5f4e7d8791ff8aa45e635129a1036e469424c84
stanley <stanley@kali.kara>
2022-10-03 08:37:26 +0100

    added file 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,9 +17,9 @@ int **alloc_grid(int width, int height)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 		{
 			*(*(arr + (width * i)) + j) = '\0';
 		}
