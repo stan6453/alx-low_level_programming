303e6e69d802cb8db056d2578a0962fc95a69359
stanley <stanley@kali.kara>
2022-10-03 07:10:31 +0100

    added file 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+
+/**
+ * _strdup - multiplies two digits
+ * @str: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+char *_strdup(char *str)
+{
+	int i, *mem;
+
+	if (str == NULL)
+		return (NULL);
+	/*find length of str*/
+	for (i = 0; str[i] != '\0'; i++)
+		i++;
+
+	mem = (char *) malloc(i + 1);
+	if (mem == NULL)
+		return (NULL);
+
+	/*copy str to mem*/
+	for (; i >= 0; --i)
+	{
+		mem[i] = str[i];
+	}
+
+}
