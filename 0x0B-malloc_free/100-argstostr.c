742fed81efbea31212b2f2acb9dd6d6c5a8b2cf3
stanley <stanley@kali.kara>
2022-10-05 17:42:49 +0100

    added file 101-strtow.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
 
 
+/**
+ * _str_len - finds length of a string
+ * @str: string to find its length
+ * Description: finds length of a string (not including null character)
+ * Return: length of string
+ */
+int _str_len(char *str){
+	int i;
+	
+	if(str == NULL)
+		return (0);
+	
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
 
 /**
  * argstostr - multiplies two digits
@@ -11,12 +27,19 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *str;
-	int i, j;
+	char *str, *head;
+	int i, j, str_len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
+	for (i = 0; i < ac; i++)
+	{
+		str_len += _str_len(av[i]);
+		str_len++; /*for the new line characters*/
+	}
+	str = malloc(srt_len + 1);
+	head = str;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
@@ -25,5 +48,5 @@ char *argstostr(int ac, char **av)
 		}
 		*str++ = '\n';
 	}
-	return (str);
+	return (head);
 }
