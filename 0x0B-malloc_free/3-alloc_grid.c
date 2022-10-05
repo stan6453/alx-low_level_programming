b99ce251349ab233c23b398f150c44a20e330667
stanley <stanley@kali.kara>
2022-10-05 03:54:20 +0100

    modified some files

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,9 +21,13 @@ int **alloc_grid(int width, int height)
 	{
 		arr[i] = (int *) malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
-			return NULL;
+		{
+			for (j = 0; j <= i; j++)
+				free(arr[j]);
+			free(arr);
+			return (NULL);
+		}
 	}
-
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
@@ -31,6 +35,5 @@ int **alloc_grid(int width, int height)
 			arr[i][j] = '\0';
 		}
 	}
-
 	return (arr);
 }
