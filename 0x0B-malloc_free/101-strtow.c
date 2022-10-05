933b8ff0c6c669e6424a6a9c41d20e174ccc4ae5
stanley <stanley@kali.kara>
2022-10-05 04:26:22 +0100

    added file 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+
+/**
+ * strtow - multiplies two digits
+ * @str: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+char **strtow(char *str)
+{
+	char *character, **str_arr;
+	int i, j, instr = 0;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+
+	}
+	return (str_arr);
+}
