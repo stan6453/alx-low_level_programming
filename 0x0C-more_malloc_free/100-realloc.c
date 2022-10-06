fe5040fcc749c8de4dbabc3e0fdfcd7b7d2a08d1
stanley <stanley@kali.kara>
2022-10-06 18:53:15 +0100

    modified file 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,7 +23,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (ptr == NULL)
-		return (malloc(new_size));
+	{
+		new_ptr = (malloc(new_size));
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
 
 	if (new_size == old_size)
 		return (ptr);
