56cacc284327e2ec2c183ac39c5bf3194c871952
stanley <stanley@kali.kara>
2022-10-06 17:53:09 +0100

    modified file 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,13 +15,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	char *ptr2 = ptr;
 
-	if (new_size == old_size)
-		return (ptr);
 	if (new_size == 0)
 	{
-		free(ptr);
+		if (ptr != NULL)
+			free(ptr);
 		return (NULL);
 	}
+
+	if (ptr == NULL)
+		return (malloc(new_size))
+
+	if (new_size == old_size)
+		return (ptr);
 
 	new_ptr = (char *) malloc(new_size);
 	if (ptr == NULL)
