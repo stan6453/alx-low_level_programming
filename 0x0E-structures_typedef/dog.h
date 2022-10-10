459a264fcb831e07fe25d561c71b7374e1fad738
stanley <stanley@kali.kara>
2022-10-10 17:50:12 +0100

    modified file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,6 +21,9 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * dog_t - Typedef for unsigned int
+ */
 typedef struct dog dog_t;
 
 void free_dog(dog_t *d);
