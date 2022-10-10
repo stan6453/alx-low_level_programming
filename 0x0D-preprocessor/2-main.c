9105d6d4c851296d543aa8bb0b02aeb3eb676097
stanley <stanley@kali.kara>
2022-10-10 17:00:22 +0100

    created all the required file for task 0x0D. C - Preprocesso

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of our c file
+ * Description: prints the name of our c file
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This folder contains my solution to the ALX project on 0x0D. C - Preprocessor
