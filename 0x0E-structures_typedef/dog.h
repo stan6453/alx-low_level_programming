7078909e2a4b71fd6231ecc2387e0ba37aec19b4
stanley <stanley@kali.kara>
2022-10-10 17:35:27 +0100

    modified file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,12 @@
 #ifndef _DOG__
 #define _DOG__
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+/*
+ * void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+*/
 
 /**
  * struct dog - Short description
