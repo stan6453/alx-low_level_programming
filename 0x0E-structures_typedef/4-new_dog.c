563e7efb88392776eed59cff15ef851484edefc0
stanley <stanley@kali.kara>
2022-10-10 21:06:18 +0100

    modified file 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,15 +12,16 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = malloc(sizeof(dog_t));
+
 	if (my_dog == NULL)
 		return (NULL);
-	my_dog->owner = malloc(sizeof(char) * (strlen(owner) +1));
+	my_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (my_dog->owner == NULL)
 	{
 		free(my_dog);
 		return (NULL);
 	}
-	my_dog->name = malloc(sizeof(char) * (strlen(name) +1));
+	my_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (my_dog->name == NULL)
 	{
 		free(my_dog);
