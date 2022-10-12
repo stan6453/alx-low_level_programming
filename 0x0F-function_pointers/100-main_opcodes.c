a2adfd3108613a86b2dcefb1a0fe94564ae15a93
stanley <stanley@kali.kara>
2022-10-12 22:11:58 +0100

    modified file 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,6 +2,25 @@
 #include <stdio.h>
 
 /**
+ * isonlydigit - checks if a string contains only digits
+ * @s: string to be checked
+ * Description: checks if a string contains only digit
+ * Return: 1 if string contains only didgit
+ *         0 if string has at lest one non-digit character
+ */
+int isonlydigit(char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+
+/**
  * main - prints the opcode of the main fn
  * @argc: the name to be printed
  * @argv: the callback function
@@ -17,6 +36,12 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		exit(1);
+	}
+
+	if (!isonlydigit(argv[1]))
+	{
+		printf("Error\n");
+		exit(3);
 	}
 
 	num = atoi(argv[1]);
