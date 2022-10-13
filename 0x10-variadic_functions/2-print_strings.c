fe9eaa90ed328621acbb94e39c7560e682e7b067
stanley <stanley@kali.kara>
2022-10-13 14:46:55 +0100

    added file 2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings- prints the opcode of the main fn
+ * @separator: the name to be printed
+ * @n: the callback function
+ * @...: the callback function
+ * Description: prints the opcode of the main fn
+ * Return: integer
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(ap, char *));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	putchar('\n');
+
+
+}
