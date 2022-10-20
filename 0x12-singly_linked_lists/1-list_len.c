2e99dc3fcba7756148887906ca2afc2495ba9f0d
stanley <stanley@kali.kara>
2022-10-20 08:40:16 +0100

    attempted task1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,18 @@
-#include <stdlib.h>
 #include "lists.h"
 
+
 /**
- * print_list - print the  content of a linked list
+ * list_len - return the lenth of a linked list
  * @h: the head of the linked list
  * Return: length of linked list
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	const list_t *temp;
 	int count = 0;
 
 	for (temp = h; temp != NULL; temp = temp->next)
 	{
-		if (temp->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", temp->len, temp->str);
 		count++;
 	}
 	return (count);
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,6 +8,17 @@ typedef struct object{
 	int len;
 	struct object *next;
 } list_t;
+
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
+
+
+
+
+
+
+
+
 
 #endif
