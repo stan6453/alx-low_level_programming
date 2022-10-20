0b1162a0397307a87c61759c03740caea494a964
stanley <stanley@kali.kara>
2022-10-20 14:03:18 +0100

    bug fix on file 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,5 @@
 0000000000001139 <main>:
     1139:       55                      push   rbp
-    113a:       48 89 e5                mov    rbp,rsp
     113d:       48 8d 05 c0 0e 00 00    lea    rax,[rip+0xec0]        # 2004 <_IO_stdin_used+0x4>
     1144:       48 89 c7                mov    rdi,rax
     1147:       e8 e4 fe ff ff          call   1030 <puts@plt>
