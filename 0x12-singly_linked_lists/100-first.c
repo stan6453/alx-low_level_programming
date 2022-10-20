eb998b403dc7845bdcab37558e2883f64299cafd
stanley <stanley@kali.kara>
2022-10-20 13:40:18 +0100

    created file 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,13 @@
 
 void housekeeping(void) __attribute__((constructor));
 
+
+
+/**
+ * housekeeping - does some action before the main function runs
+ */
 void housekeeping(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
