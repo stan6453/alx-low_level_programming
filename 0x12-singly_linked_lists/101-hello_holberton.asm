7cd5c39c87e13bae0795246f5c96202800a3d1e6
stanley <stanley@kali.kara>
2022-10-21 20:43:09 +0100

    bug fix on file 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,29 +1,25 @@
-	.file	"101-hello_holberton.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rdi
-	call	puts@PLT
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Debian 11.3.0-5) 11.3.0"
-	.section	.note.GNU-stack,"",@progbits
+; A simple "Hello, Holberton\n" program
+;
+; Author: Stanley Ezechukwu
+; Date:   Fri, Oct 21, 2022
 
+global _start
+
+section .text:
+
+_start:
+	mov eax, 0x4		; use the write syscall
+	mov ebx, 1		; use stdout as the file descriptor
+	mov ecx, message	; use the message sa the buffer
+	mov edx, message_length	; supply the message length
+	int 0x80		; invoke the syscall
+
+	; now gracefully exit
+
+	mov eax, 0x1
+	mov ebx, 0
+	int 0x80
+
+section .data:
+	message: db "Hello, Holberton", 0xA
+	message_length equ $-message
