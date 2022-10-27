7d6523bea9b236b35302c563b4771114ed1d3c81
stanley <stanley@kali.kara>
2022-10-27 22:45:27 +0100

    fixed betty error in file 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,11 @@
 /**
  * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
 	unsigned int i = 1;
-
 	char *c = (char *)&i;
+
 	return (*c);
 }
