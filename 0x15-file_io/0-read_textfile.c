9ac3e444e6f6842ba93f5c3cb66b177bc610a079
stanley <stanley@kali.kara>
2022-10-31 13:05:16 +0100

    included files for system calls

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if ((fd = open(filename, O_RDONLY)) == -1)
 		return (0);
 
-	 if((n = read(fd, buf, letter)) == -1)
+	 if((n = read(fd, buf, letters)) == -1)
 		 return (0);
 
 	 if (write(1, buf, n) == -1)
