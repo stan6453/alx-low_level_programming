05403845d016c4923d2f63be04b5b0ef71970169
stanley <stanley@kali.kara>
2022-10-31 14:17:18 +0100

    fixed error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,8 +6,9 @@ int _str_len(char *str);
  * create_file - creates a file
  * @filename: name of file to create
  * @text_content:  a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure 
- * (file can not be created, file can not be written, write “fails”, etc…)
+ * Return: 1 on success, -1 on failure
+ * (file can not be created, file can
+ * not be written, write “fails”, etc…)
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -20,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		return (0);
 	n = _str_len(text_content);
 	if (write(fd, text_content, n) == -1)
-		 return (0);
+		return (0);
 	close(fd);
 	return (1);
 }
