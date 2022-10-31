64f4847e9bed8a962bc294048672daf25079e651
stanley <stanley@kali.kara>
2022-10-31 14:18:49 +0100

    fixed error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, n;
 
 	if (filename == NULL || text_content == NULL)
-		return (0);
+		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
