1ca8d3c8b18cb53374e93d297ea955c0e69bd110
stanley <stanley@kali.kara>
2022-10-31 14:32:10 +0100

    fixed error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,8 +5,11 @@ int _str_len(char *str);
 /**
  * append_text_to_file - appends text at the end of a file.
  * @filename: the name of the file
- * @text_content:  a NULL terminated string to add at the end of the file
- * Return: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…)
+ * @text_content:  a NULL terminated
+ * string to add at the end of the file
+ * Return: 1 on success, -1 on failure
+ * (file can not be created, file
+ * can not be written, write “fails”, etc…)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -19,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	n = _str_len(text_content);
 	if (write(fd, text_content, n) == -1)
-		 return (-1);
+		return (-1);
 	close(fd);
 	return (1);
 }
@@ -38,7 +41,7 @@ int _str_len(char *str)
 {
 	int i;
 
-	if(str == NULL)
+	if (str == NULL)
 		return (0);
 
 	for (i = 0; str[i] != '\0'; i++)
