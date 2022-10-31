c1ee43f506f05099fecd8c32776b203615359b0a
stanley <stanley@kali.kara>
2022-10-31 14:35:17 +0100

    fixed error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, n;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_APPEND);
 	if (fd == -1)
