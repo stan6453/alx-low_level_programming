6ab80e95380379ba9bf49064b55b6e30dc8ba8f2
stanley <stanley@kali.kara>
2022-11-01 00:19:08 +0100

    fixed error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd1 = openfile1(argv[1]);
