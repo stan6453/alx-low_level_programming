ace8dcee7414cc988419f3fc111eb0907339820a
stanley <stanley@kali.kara>
2022-10-31 14:23:16 +0100

    fixed error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	n = _str_len(text_content);
 	if (text_content == NULL)
-
+		;
 	else if (write(fd, text_content, n) == -1)
 		return (-1);
 	close(fd);
