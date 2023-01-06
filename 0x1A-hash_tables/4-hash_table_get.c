5cc1b37205f794825a339346550214f164e8a6ec
stanley <stanley6453@gmail.com>
2023-01-06 15:10:43 +0100

    created 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,7 +5,8 @@
  * hash_table_get -  retrieves a value associated with a key.
  * @ht: the hash table to look into
  * @key: the key you are looking for
- * Return: the value associated with the element, or NULL if key couldn’t be found
+ * Return: the value associated with the
+ * element, or NULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
