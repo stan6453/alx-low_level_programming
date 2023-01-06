64c7fbb7219ad20951f7b22a6c5c386087c94eb3
stanley <stanley6453@gmail.com>
2023-01-06 20:21:25 +0100

    created file 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,6 +9,8 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
+	int has_next = 0;
+	int first_item = 1;
 
 	if (ht == NULL)
 	{
@@ -20,10 +22,13 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		if (ht->array[i] != NULL)
 		{
+			has_next = 1;
+			if (has_next && !first_item)
+				printf(", ");
 			printf("'%s':", ht->array[i]->key);
 			printf(" '%s'", ht->array[i]->value);
-
-			printf(", ");
+			first_item = 0;
+			has_next = 0;
 		}
 	}
 
