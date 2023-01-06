9e61dd84ccc3831b722836c99a7cc98201430147
stanley <stanley6453@gmail.com>
2023-01-06 15:11:34 +0100

    created 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -19,7 +19,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 
-	while(temp != NULL)
+	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
@@ -27,5 +27,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		}
 		temp = temp->next;
 	}
-	return NULL;
+	return (NULL);
 }
