9a6fb486bae67d85ee4202a4a03d893d4f140c81
stanley <stanley6453@gmail.com>
2023-01-06 14:53:50 +0100

    created 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 
-
+void free_node(hash_node_t *node);
 /**
  * hash_table_set - adds an element to the hash table.
  * @ht: the hash table to add or update the key/value to
@@ -19,45 +19,36 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || key[0] == '\0' || ht == NULL || value == NULL)
 		return (0);
-
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
-	node->key = malloc(strlen(key) + 1);
+	node->key = strdup(key);
 	if (node->key == NULL)
 	{
 		free(node);
 		return (0);
 	}
-	node->value = malloc(strlen(value) + 1);
+	node->value = strdup(value);
 	if (node->value == NULL)
 	{
 		free(node->key);
 		free(node);
 		return (0);
 	}
-	strcpy(node->key, key);
-	strcpy(node->value, value);
 	node->next = NULL;
-
-
 	index = key_index((const unsigned char *)key, ht->size);
-
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = node;
 		return (1);
 	}
-
 	temp = ht->array[index];
-	while(temp != NULL)
+	while (temp != NULL)
 	{
-		if(strcmp(temp->key, node->key) == 0)
+		if (strcmp(temp->key, node->key) == 0)
 		{
 			strcpy(temp->value, node->value);
-			free(node->key);
-			free(node->value);
-			free(node);
+			free_node(node);
 			return (1);
 		}
 		temp = temp->next;
@@ -66,4 +57,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->next = temp;
 	ht->array[index] = node;
 	return (1);
+}
+
+/**
+ * free_node - fress a lonely node (not attched to any linked list)
+ * @node: node to be freed
+ */
+void free_node(hash_node_t *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
 }
