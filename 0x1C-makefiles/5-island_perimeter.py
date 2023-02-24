b5a4434a76a4ad309e36c6876ac9e813c64097a4
stanley <stanley6453@gmail.com>
2023-02-24 19:47:39 +0100

    modified the file 0x1C-makefiles/5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,18 +12,26 @@ def island_perimeter(grid):
     x2 = 0
     for ycount, y in enumerate(grid):
         if y1 == 0 and 1 in y:
-            y1 = ycount
+            y1 = ycount + 1
         if 1 in y:
-            y2 = ycount
+            y2 = ycount + 1
 
         for xindex, x in enumerate(y):
             if x == 1:
                 if x2 < xindex:
-                    x2 = xindex
+                    x2 = xindex + 1
                 if x1 > xindex:
-                    x1 = xindex
+                    x1 = xindex + 1
+
+    if x1 == 100:
+        x1 = 0
 
     ylength = (y2 - y1) + 1
     xlength = (x2 - x1) + 1
+
+    if x1 == 0 and x2 == 0:
+        xlength = 0
+    if y1 == 0 and y2 == 0:
+        ylength = 0
 
     return (ylength * 2) + (xlength * 2)
