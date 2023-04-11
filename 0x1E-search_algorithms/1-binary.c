75328823a177130d36405f51bb0885ca3c6d0009
stanley <stanley6453@gmail.com>
2023-04-11 13:06:11 +0100

    added file 0x1E-search_algorithms/README.md

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,6 @@
 #include "search_algos.h"
-int binary_search_helper(int *array, size_t size, int value, size_t index_offset);
+int binary_search_helper(int *array, size_t size,
+						 int value, size_t index_offset);
 
 /**
  * binary_search - searches for a value in a sorted array of integers
@@ -21,11 +22,13 @@ int binary_search(int *array, size_t size, int value)
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
- * @index_offset: offset of first element from index 0
+ * @index_offset: offset of first element
+ * of the subarray from index 0 of the whole array
  * Return: index where the value is located.
  *	   -1 if value is not present in array, or if array is NULL.
  */
-int binary_search_helper(int *array, size_t size, int value, size_t index_offset)
+int binary_search_helper(int *array, size_t size,
+						 int value, size_t index_offset)
 {
 	size_t mid_index;
 	size_t new_size;
@@ -33,7 +36,6 @@ int binary_search_helper(int *array, size_t size, int value, size_t index_offset
 	if (array == NULL)
 		return (-1);
 
-	printf("size: %ld ", size);
 	printf("Searching in array: ");
 	printarray(array, size);
 
@@ -42,13 +44,15 @@ int binary_search_helper(int *array, size_t size, int value, size_t index_offset
 	{
 		return (mid_index + index_offset);
 	}
-	else if (size == 1){
+	else if (size == 1)
+	{
 		return (-1);
 	}
 	else if (array[mid_index] < value)
 	{
 		new_size = (size % 2) == 0 ? size / 2 : size / 2;
-		return (binary_search_helper((array + mid_index + 1), new_size, value, index_offset + mid_index + 1));
+		return (binary_search_helper((array + mid_index + 1),
+									 new_size, value, index_offset + mid_index + 1));
 	}
 	else
 	{
@@ -69,7 +73,7 @@ void printarray(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		if (i != size - 1)
-			printf("%d,", array[i]);
+			printf("%d, ", array[i]);
 		else
 			printf("%d\n", array[i]);
 	}
