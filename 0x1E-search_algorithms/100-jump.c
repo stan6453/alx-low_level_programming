7ade36e3405e11f5a96a3dfb0ceea0816ab8c285
stanley <stanley6453@gmail.com>
2023-04-12 14:21:51 +0100

    modified file 0x1E-search_algorithms/100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -27,6 +27,8 @@ int jump_search(int *array, size_t size, int value)
 	if (array[index] == value && index == 0)
 	{
 		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+		printf("Value found between indexes [%ld] and [%ld]\n", index, index);
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
 		return (index);
 	}
 
